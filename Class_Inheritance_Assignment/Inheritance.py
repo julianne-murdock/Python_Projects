0877de68b6690581dbefceeef2f52165bc9621eb
julianne-murdock <98274277+julianne-murdock@users.noreply.github.com>
2022-05-13 07:34:41 -0400

    finished Inheritance

---
+++ Class_Inheritance_Assignment/Inheritance.py
@@ -0,0 +1,19 @@
+#
+# Author: Julianne Murdock
+#
+# Date: May 13th 2022
+#
+# Purpose: Create two classes that inherit from a another class
+#
+
+class Student:
+    name = "Display name here"
+    student_id = '123456'
+
+class Video_Game_Designer(Student):
+    favorite_game = 'Subway surfers'
+    Current_Company = 'Apple'
+
+class Software_Developer(Student):
+    Graduation_Date_From_TTA = ''
+    Linkedin_Profile_URL = ''
