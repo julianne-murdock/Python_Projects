e16a34eaf2ce4a7e020aabea9a52989344d7e568
julianne-murdock <98274277+julianne-murdock@users.noreply.github.com>
2022-05-18 16:34:14 -0400

    updated bugs

--- Object_Oriented_Classes/Polymorphism.py
+++ Object_Oriented_Classes/Polymorphism.py
@@ -7,49 +7,51 @@
 #
 
 class Art_World:
-    Instagram_username = 'rising.horizons'
-    Artist = 'Julianne Murdock'
-    Password = '12345678910hackme'
+    username = 'rising.horizons'
+    artist = 'Julianne Murdock'
+    password = '12345678910hackme'
 
     def getArtWorldLogin(self):
-        login_name = input('Enter your First and Last name here ')
+        login_name = input('Enter your Art World Username')
         login_password = input('Enter your Art World Password ')
-        if (login_name == self.name and login_password == self.password):
-            print("Welcome, {} to Art World! I can't wait to see what you create!".format(entry_name))
+        if (login_name == self.username and login_password == self.password):
+            print("Welcome, {} to Art World! I can't wait to see what you create!".format(self.artist))
         else:
             print('A wrong Name or Password was entered. Please try again.')
 #child class for Art Gallery
-class Art_Gallery(Art_World):
-    Experience = 'Sponge Paintings with Jellyfish Jelly'
+class art_gallery(Art_World):
+    experience = 'Sponge Paintings with Jellyfish Jelly'
     name = 'Spongebob Squarepants'
-    Expected_Graduation = 'Unknown, Student for 20 years'
-    Student_ID = '15963'
+    password = 'jellyfish'
+    username = 'spongey'
+    expected_Graduation = 'Unknown, Student for 20 years'
+    student_ID = '15963'
 
-    #this will use the same method from the prior parent class but using the entry pin
+    #this will use the same method from Art_World class but using the login_ID
     def getArtWorldLogin(self):
-         login_name = input('Enter your First and Last name here:   ')
-         login_password = input('Enter your Art World Password:  ')
+         login_name = input('Enter your Art Gallery Username')
+         login_password = input('Enter your Art Gallery Password:  ')
          login_ID = input('Enter your Student ID: ')
-         if (login_name == self.name and _pin == self.login_ID):
-             print("Welcome back to Art World, {}. I can't wait to see the new things you create today!".format(entry_name))
+         if (login_name == self.username and login_ID == self.student.ID and login_password == self.password):
+             print("Welcome back to Art World, {}. I can't wait to see the new things you create today!".format(self.name))
 
 
 #second child class for Art Club
 class Art_Club(Art_World):
     monthly_fee = 5.00 #footlong
     music_of_the_day = 'ghostbusters'
-    Art_Challenge = 'Not Sponge Paintings'
+    art_challenge = 'Not Sponge Paintings'
 
     def getArtWorldLogin(self):
-         login_name = input('Enter your First and Last name here:   ')
-         login_password = input('Enter your Art World Password:  ')
+         login_name = input('Enter your Art Gallery Username')
+         login_password = input('Enter your Art Gallery Password:  ')
          login_ID = input('Enter your Student ID: ')
-         if (login_name == self.name and _pin == self.login_ID):
-             print("Welcome back to Art Club, {}. It is time to take on the challenge of the day!".format(entry_name))
+         if (login_name == self.username and login_ID == self.student.ID and login_password == self.password):
+             print("Welcome back to Art Club, {}. It is time to take on the challenge of the day!".format(self.name))
 
 
-Art_Club = Art_World()
-Art_Club.getArtWorldLogin
+artNow = Art_World()
+artNow.getArtWorldLogin()
 
 
 
