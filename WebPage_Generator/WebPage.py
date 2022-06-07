f28c7217e78e71efb8f9bb40c28e855777d809a2
julianne-murdock <98274277+julianne-murdock@users.noreply.github.com>
2022-06-07 16:44:45 -0400

    fixed background

--- WebPage_Generator/WebPage.py
+++ WebPage_Generator/WebPage.py
@@ -20,7 +20,7 @@ class ParentWindow(Frame):
         self.master.title('Enter the title of the window here!') 
         self.master.config(bg='lightblue')
 
-        self.txtWeb = Entry(self.master,text="web text", font=('Ariel', 11,), fg = 'black', bg='CBC3E3')  
+        self.txtWeb = Entry(self.master,text="web text", font=('Ariel', 11,), fg = 'black', bg='#CBC3E3')  
         self.txtWeb.grid(row=1, column=1, padx=(180,0), pady=(50,0)) 
 
         self.btnSubmit = Button(self.master, text ="Submit", width= 11, height=2, command=self.webGenerator) 
